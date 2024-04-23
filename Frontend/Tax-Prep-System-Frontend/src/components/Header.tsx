369a7bcd680440da84a65e3f44b064bd451fa284
tylercausey <tylercausey1219@gmail.com>
2024-04-23 16:24:16 -0500

    updated the organization of the frontend files, added a header

---
+++ Frontend/Tax-Prep-System-Frontend/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import "./header.css"
+
+const Header = () => {
+
+    return(
+        <header>
+            <img id="flagImage" src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png?20151118161041" alt="Flag of the United States" />
+            <div id="headerText"> Federal 1099-INT Calculator </div>
+        </header>
+    )
+}
+
+export default Header
